761190f6e7bd0b2601da69d76af2dc6fa7ce80a2
Divyansh2-droid <pandeydivyansh.up53@gmail.com>
2025-09-09 12:26:43 +0530

    Time: 1 ms (63.35%), Space: 41 MB (38.93%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -2,19 +2,21 @@ class Solution {
     public boolean isHappy(int n) {
         Set<Integer> usedIntegers = new HashSet<>();
         while(true){
+            // finding sumnof the squares
             int sum =0;
             while(n!=0){
                 sum+= Math.pow(n%10, 2.0);
                 n = n/10;
             }
-
-
+       
+            //if sum equals 1 returns true
             if(sum==1) return true;
-
+           //else our new number ie n = sum
             n=sum;
-
+          //if hashset contains the number n return false because wapis usi number pe aa gye loop me fas gye so get out of loop by returning false
             if(usedIntegers.contains(n))
             return false;
+            //else agar number hashset me nahi hai toh usko ab hashset me add krdo
             usedIntegers.add(n);
         }
     }
